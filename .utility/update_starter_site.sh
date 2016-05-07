03cf6b31cb45379ffb72c3b448b1336f5e86da62
Kevzlou7979 <kevzlou7979@gmail.com>
2016-05-07 14:43:56 +0800

    Updated Travis

--- .utility/update_starter_site.sh
+++ .utility/update_starter_site.sh
@@ -24,23 +24,23 @@ git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/GwtMaterialDesi
 cd gh-pages
 
 # remove the GwtMaterialStarter directories from git.
-if [[ -d ./GWTMaterialStarter ]]; then
-git rm -rf ./GWTMaterialStarter
+if [[ -d ./snapshot/GWTMaterialStarter ]]; then
+git rm -rf ./snapshot/GWTMaterialStarter
 fi
-if [[ -f ./index.html ]]; then
-git rm -rf ./index.html
+if [[ -f ./snapshot/index.html ]]; then
+git rm -rf ./snapshot/index.html
 fi
-if [[ -d ./META-INF ]]; then
-git rm -rf ./META-INF
+if [[ -d ./snapshot/META-INF ]]; then
+git rm -rf ./snapshot/META-INF
 fi
 if [[ -d ./WEB-INF ]]; then
-git rm -rf ./WEB-INF
+git rm -rf ./snapshot/WEB-INF
 fi
 
 # copy the new GwtMaterialStarter the snapshot dir.
 unzip -u $TRAVIS_BUILD_DIR/target/gwt-material-template-*.war -d ./
-rm -rf ./META-INF
-rm -rf ./WEB-INF
+rm -rf ./snapshot/META-INF
+rm -rf ./snapshot/WEB-INF
 
 git add -f .
 git commit -m "Auto-push demo to gh-pages successful. (Travis build: $TRAVIS_BUILD_NUMBER)"
